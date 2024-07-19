565454c5ad0e051c137fe3208c7005d6c42a18eb
Priyanka <pbhaukajee@gmail.com>
2024-07-19 15:45:52 -0500

    add data section

---
+++ vite-react/src/data.jsx
@@ -0,0 +1,38 @@
+import Pokhara from "./images/pokhara.png";
+import Newyork from "./images/newyork.png";
+import Zion from "./images/zion.png";
+
+export default [
+  {
+    title: "Pokhara",
+    location: "NEPAL",
+    googleMapsUrl: "https://goo.gl/maps/ZJimDY3Pr5Lj3b2r9",
+    startDate: "12 Dec, 2022",
+    endDate: "15 Dec, 2022",
+    description:
+      "Pokhara is the gateway to the Annapurna region where many a trekker finds their Shangri-La. Its pristine air, the spectacular backdrop of the snowy peaks of the Annapurna Range and the serene Phewa, Begnas and Rupa Lakes, makes this destination ‘the Jewel of the Himalaya’.",
+    img: Pokhara,
+  },
+
+  {
+    title: "Newyork City",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/1SBfqUq8wCPrydF8A",
+    startDate: "18 Mar, 2022",
+    endDate: "20 Mar, 2022",
+    description:
+      "New York is a city made up of five boroughs: Manhattan, Brooklyn, Queens, the Bronx, and Staten Island. Cool, cosmopolitan, crowded, constantly evolving … the Big Apple blends big city splendor with small-town charm. Whatever it is you're into, you'll find there's more to do in New York than you'll have time.",
+    img: Newyork,
+  },
+
+  {
+    title: "Zion National Park",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/KqJcFsTJHGSpfYMn6",
+    startDate: "5 Jul, 2022",
+    endDate: "7 Jul, 2022",
+    description:
+      "A thousand-foot deep red rock canyon, nested in dramatic surroundings. Whether you are trying to catch your breath while climbing the trail to Observation Point or watching the shadows constantly change the mood of the Court of the Patriarchs, Zion National Park is always ready to quench your appetite for outdoor wonder.",
+    img: Zion,
+  },
+];
